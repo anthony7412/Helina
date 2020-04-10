b1cbfeebfef82e02f78610636fece7e83e4f7e29
hidri <agthidri2400@gmail.com>
2020-04-10 20:35:42 +0300

    this commit will add the footer

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$ (function(){
+    new WOW().init();   
+});
+$(window).on('load',function(){
+    $("#").addClass("animated fadeInDown");
+    $("#").addClass("animated fadeInRight");
+    $("#").addClass("animated zoomIn");
+    $("#").addClass("animated fadeDown infinite");
+
+});
